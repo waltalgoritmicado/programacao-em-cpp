2a889c3d8393aa8a731589c3bc7c87d9406edccb
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-12 13:07:51 -0300

    funcoes

---
+++ capitulo-2/funcoes.cpp
@@ -0,0 +1,12 @@
+// exemplo de funcoes definidas pelo usuario
+#include <iostream>
+using namespace std;
+
+void visualizar(){
+    cout << "Olá Andrieli, topa sair comigo?\n";
+}
+
+int main() {
+    visualizar();
+    return 0;
+}
