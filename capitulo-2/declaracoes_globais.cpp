cb5eaad3ac9869290aedd70df07b267e5972e8fb
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-12 12:36:05 -0300

    declaracoes globais

---
+++ capitulo-2/declaracoes_globais.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// definir macros
+#define MINCONST1 0.50
+#define MINCONST2 0.75
+
+// declaracoes globais
+int grau_classe;
+
+// prototipos
+int media(int a, int b);
+
+int main(){
+    cout << "declaracoes globais \n";    
+
+    return 0;
+}
