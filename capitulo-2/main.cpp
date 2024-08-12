f52f896bada6f58517f2ce1492fe70767ce92901
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-12 12:23:56 -0300

    funcao main alternativa

--- capitulo-2/main.cpp
+++ capitulo-2/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int main(void) {
+    cout << "Funcao main com void \n";
+    return 0;
+}
+
+/*
 int main () {
 	cout << "Funcao main\n";
     return 0;
 }
+*/
