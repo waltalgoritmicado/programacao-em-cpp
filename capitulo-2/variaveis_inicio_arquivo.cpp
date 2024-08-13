85e090281f9f9a116f93e415370277f73a25c0dc
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-13 14:04:48 -0300

    variaveis

---
+++ capitulo-2/variaveis_inicio_arquivo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int mi_numero;
+
+int main() {
+	cout << "Entre o seu número favorito\? ";
+	cin >> mi_numero;
+	return 0;
+}
---
+++ capitulo-2/variaveis_ponto_utilizacao.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	for (i= 0; i< 10; i++){
+		cout << "Hello World\n";
+	}
+	return 0;
+}
