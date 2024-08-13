d15ae0b3ff5d4b543df1aa28a2b7c0a912d6fa94
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-13 14:47:46 -0300

    E/S

---
+++ capitulo-2/cin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	double x;
+
+	cin	>> n
+		>> x; 
+
+	return 0;
+}
---
+++ capitulo-2/cout.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Olá Mundo " << "Sou Walt\n";
+	cout << "Bem-vindo\nao\nmundo\nda\nprogramação\n";
+	return 0;
+}
