6d1f93c0ded4c0c8ec05b6ab16cf17eb7db11c67
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-08-30 02:23:13 -0300

    up

---
+++ capitulo-3/incremento_decremento.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int m = 45, n = 75;
+	cout << "m = " << m << " n = " << n << endl;
+	++m;
+	--n;
+	cout << "m = " << m << " n = " << n << endl;
+	m++;
+	n--;
+	cout << "m = " << m << " n = " << n << endl;
+	return 0;
+}
---
+++ capitulo-3/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bem vindo ao capitúlo 3 do Livro Programação em C++\n";
+    return 0;
+}
